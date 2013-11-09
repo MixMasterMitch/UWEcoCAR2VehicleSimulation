2c0cd04d379bc5d457f8fd4ec2575782abf79baf
Nicholas Clawson <nickclaw@gmail.com>
2013-11-08 17:28:35 -0800

    Created MalibuStepSimulator.java
    Moved code away from EntryPoint.java to make an object in charge of
    simulating single time steps of the car.

--- src/EntryPoint.java
+++ src/EntryPoint.java
@@ -10,46 +10,9 @@ public class EntryPoint {
     // The amount of time to run the simulation for.
     public static final Double SIMULATION_LENGTH = 10.0; // s
 
-    // The weight of the UW EcoCAR 2013 Chevy Malibu
-    public static final Double MALIBU_WEIGHT = 2200.0; // kg
-
-    // Gravitational Acceleration on Earth
-    public static final Double GRAVITY = 9.81; // m/s^2
-
-    // The weight of the UW EcoCAR 2013 Chevy Malibu
-    public static final Double MALIBU_DOWN_FORCE = MALIBU_WEIGHT * GRAVITY; // N
-
-    // The effective radius of the tires
-    public static final Double TIRE_RADIUS = .32; // m
-
-    // The density of air at STP
-    public static final Double AIR_DENSITY = 1.2; // kg / m^3
-
-    // The coefficient of air resistance of the 2013 Chevy Malibu
-    public static final Double MALIBU_AIR_DRAG_COEFF = .29;
-
-    // The coefficient of rolling resistance of the tires on the road
-    public static final Double MALIBU_ROLLING_COEFF = .02;
-
-    // The frontal area of the 2013 Chevy Malibu (i.e. the cross sectional area)
-    public static final Double MALIBU_FRONTAL_AREA = 2.15; // m^2
-
     public static final DecimalFormat DF = new DecimalFormat("0000.0");
 
     public static void main(String[] args) throws Exception {
-
-        // The forward speed of the vehicle
-        Double speed = 0.0; // m/s
-
-        // The trq being applied to the wheels by the engine
-        Double engineTrq = 0.0; // Nm
-
-        // The distance traveled (i.e. integral of speed)
-        Double distance = 0.0; // m
-
-        // TODO: convert gasSum into actual gas usage
-        // The total amount of gas used (i.e. integral of gas)
-        Double gasSum = 0.0; // %
 
         // Create a lookup table defining trq at the wheels from the engine
         // for a given gas pedal position and wheel rpm.
@@ -64,97 +27,30 @@ public class EntryPoint {
         gas.put(5.0, 50.0);
         gas.put(7.0, 0.0);
         LookUpTable1D lookUpTable1D = new LookUpTable1D(gas);
+        
+        MalibuStepSimulator simulator = new MalibuStepSimulator(lookUpTable1D, gasRpmTrq, TIME_STEP);
 
         // Run the simulation
-        for (double simulationTime = 0.0; simulationTime < SIMULATION_LENGTH; simulationTime += TIME_STEP) {
-            /*
-                STEP 1: Get input values
-             */
-            Double gasPedalPosition = lookUpTable1D.getValue(simulationTime);
-
-            /*
-                STEP 2: Calculate speed / rpm
-             */
-
-//            System.out.println("ENGINE FORCE " + getWheelForce(trq));
-//            System.out.println("WIND RESISTANCE " + getAirResistance(speed));
-//            System.out.println("ROLLING RESISTANCE " + getRollingResistance(speed));
-
-            // TODO: include vehicle rotational inertia calculations
-            speed += (getWheelForce(engineTrq) - getRollingResistance(speed) - getAirResistance(speed)) / MALIBU_WEIGHT * TIME_STEP;
-
-            // TODO: negative values cause problems
-            speed = Math.max(speed, 0);
-
-            Double rpm = convertSpeedToRpm(speed, TIRE_RADIUS);
-
-            /*
-                STEP 3: Calculate Torques
-             */
-            engineTrq = gasRpmTrq.getValue(gasPedalPosition, rpm);
-
-            /*
-                STEP 4: Integrate
-             */
-            distance += speed * TIME_STEP;
-            gasSum += gasPedalPosition * TIME_STEP;
-
-            /*
-                STEP 5: Display time step data
-             */
+        while (simulator.getSimulationTime() < SIMULATION_LENGTH) {
+        	// get gas input before step
+        	Double pedalPosition = simulator.getGasPedalPosition();
+        	
+        	// simulate one TIME_STEP
+        	simulator.step();
+        	
+        	// output 
             System.out.println(
-                    "At time[s]: " + DF.format(simulationTime) +
-                            " gas[%]: " + DF.format(gasPedalPosition) +
-                            " speed[mph]: " + DF.format(convertMpsToMph(speed)) +
-                            " rpm: " + DF.format(rpm) +
-                            " trq[Nm]: " + DF.format(engineTrq) +
-                            " distance[m]: " + DF.format(distance));
+                    "At time[s]: " + DF.format(simulator.getSimulationTime()) +
+                            " gas[%]: " + DF.format(pedalPosition) +
+                            " speed[mph]: " + DF.format(convertMpsToMph(simulator.getSpeed())) +
+                            " rpm: " + DF.format(simulator.getRpm()) +
+                            " trq[Nm]: " + DF.format(simulator.getTorque()) +
+                            " distance[m]: " + DF.format(simulator.getDistance()));
         }
 
         // Output simulation aggregate results
-        System.out.println("Total Gas Used: " + gasSum);
+        System.out.println("Total Gas Used: " + simulator.getGasSum());
 
-    }
-
-    /**
-     * Calculates the air resistance applied to the vehicle at the given speed
-     * @param speed m/s
-     * @return A positive number means a negative force
-     */
-    public static Double getAirResistance(Double speed) { // m/s
-
-        return MALIBU_AIR_DRAG_COEFF * AIR_DENSITY * MALIBU_FRONTAL_AREA * speed * speed / 2; // F = c 1/2 ρ v2 A
-    }
-
-    /**
-     * Calculates the rolling resistance of the vehicle at the given speed
-     * @param speed m/s
-     * @return A positive number means a negative force
-     */
-    public static Double getRollingResistance(Double speed) { // m/s
-        // TODO: Handle reverse
-        if (Math.round(speed*100)/100.0 > 0.0) { // We are going faster than .0005 m/s
-            return MALIBU_DOWN_FORCE * MALIBU_ROLLING_COEFF; // F = c W
-        } else {
-            return 0.0;
-        }
-    }
-
-    /**
-     * Calculates the force being applied to the vehicle by the given axel torque
-     * @param trq Nm
-     */
-    public static Double getWheelForce(Double trq) {
-        return trq / TIRE_RADIUS; // Nm / m -> N
-    }
-
-    /**
-     * Calculates the rpm that a wheel of the given radius would spin with a linear velocity of the given speed.
-     * @param speed m/s
-     * @param radius m
-     */
-    public static Double convertSpeedToRpm(Double speed, Double radius) {
-        return speed / (2 * radius * Math.PI) * 60;
     }
 
     /**
---
+++ src/MalibuStepSimulator.java
@@ -0,0 +1,183 @@
+public class MalibuStepSimulator {
+	
+	/************ CONTANT VARIABLES ************/
+	
+	 // The weight of the UW EcoCAR 2013 Chevy Malibu
+    private static final Double MALIBU_WEIGHT = 2200.0; // kg
+
+    // Gravitational Acceleration on Earth
+    private static final Double GRAVITY = 9.81; // m/s^2
+
+    // The weight of the UW EcoCAR 2013 Chevy Malibu
+    private static final Double MALIBU_DOWN_FORCE = MALIBU_WEIGHT * GRAVITY; // N
+
+    // The effective radius of the tires
+    private static final Double TIRE_RADIUS = .32; // m
+
+    // The density of air at STP
+    private static final Double AIR_DENSITY = 1.2; // kg / m^3
+
+    // The coefficient of air resistance of the 2013 Chevy Malibu
+    private static final Double MALIBU_AIR_DRAG_COEFF = .29;
+
+    // The coefficient of rolling resistance of the tires on the road
+    private static final Double MALIBU_ROLLING_COEFF = .02;
+
+    // The frontal area of the 2013 Chevy Malibu (i.e. the cross sectional area)
+    private static final Double MALIBU_FRONTAL_AREA = 2.15; // m^2
+    
+    /************ CUSTOM VARIABLES ************/
+    
+    // The amount of time between simulation steps. Lower number = more accurate and slower
+    private final Double time_step;
+    
+    // A table of mapping time to gas input
+    private final LookUpTable1D gas;
+    
+    // the lookup table
+    private final LookUpTable2D gasRpmTrq;
+    
+    /************ DYNAMIC VARIABLES ************/
+    
+    // The number of steps the simulation has run for
+    private int steps;
+    
+    // the current speed of the car
+    private Double speed;
+    
+    // the total distance the car has traveled
+    private Double distance;
+    
+    // the total amount of gas used
+    private Double gasSum;
+        
+	public MalibuStepSimulator(LookUpTable1D gas, LookUpTable2D gasRpmTrq, Double time_step) {
+		this.gas = gas;
+		this.gasRpmTrq = gasRpmTrq;
+		this.time_step = time_step;
+		
+		this.steps = 0;
+		this.speed = 0.0;
+		this.distance = 0.0;
+		this.gasSum = 0.0;
+	}
+	
+	/**
+	 * Step forward one time_step in the simulation
+	 */
+	public void step() {
+		Double pedalPosition = this.getGasPedalPosition();
+			
+		// Calculate speed / rpm
+		this.speed += (this.getWheelForce() - this.getRollingResistance() - this.getAirResistance())/ this.MALIBU_WEIGHT * this.time_step;
+		this.speed = Math.max(this.speed, 0);
+				
+		this.distance += this.speed * this.time_step;
+		this.gasSum += pedalPosition * this.time_step;
+		
+		this.steps++;
+	}
+	
+	/**
+	 * Step backwards oen time_step in the simulation
+	 * @throws UnsupportedOperationException();
+	 */
+	public void unstep() {
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Calculate the simulation time
+	 * @return
+	 */
+	public Double getSimulationTime() {
+		return this.getSteps() * this.time_step;
+	}
+	
+	/**
+	 * Calculate the current gas pedal position
+	 * @return
+	 */
+	public Double getGasPedalPosition() {
+		return this.gas.getValue(this.getSimulationTime());
+	}
+	
+	/**
+	 * Get the current speed of the car
+	 * @return
+	 */
+    public Double getSpeed() {
+		return speed;
+	}
+    
+    /**
+     * Calculate the current torque exerted by the engine(?)
+     * @return
+     */
+	public Double getTorque() {
+		return this.gasRpmTrq.getValue(this.getGasPedalPosition(), this.getRpm());
+	}
+	
+	/**
+	 * Get the current total distance traveled
+	 * @return
+	 */
+	public Double getDistance() {
+		return distance;
+	}
+	
+	/**
+	 * Get the current amount of gas used
+	 * @return
+	 */
+	public Double getGasSum() {
+		return gasSum;
+	}
+	
+	 /**
+     * Calculates the force being applied to the vehicle by the given axel torque
+     * @return the current force exerted by the wheel on the road
+     */
+    public Double getWheelForce() {
+        return this.getTorque() / this.TIRE_RADIUS; // Nm / m -> N
+    }
+    
+    /**
+     * Calculates the rolling resistance of the vehicle at the given speed
+     * @return A positive number means a negative force
+     */
+    public Double getRollingResistance() { // m/s
+        // TODO: Handle reverse
+        if (Math.round(this.speed*100)/100.0 > 0.0) { // We are going faster than .0005 m/s
+            return this.MALIBU_DOWN_FORCE * this.MALIBU_ROLLING_COEFF; // F = c W
+        } else {
+            return 0.0;
+        }
+    }
+    
+    /**
+     * Calculates the air resistance applied to the vehicle at the given speed
+     * @return A positive number means a negative force
+     */
+    public Double getAirResistance() { // m/s
+
+        return this.MALIBU_AIR_DRAG_COEFF * this.AIR_DENSITY * this.MALIBU_FRONTAL_AREA * Math.pow(this.speed, 2) / 2; // F = c 1/2 ρ v2 A
+    }
+    
+    /**
+     * Calculate the RPM of the car wheels
+     * @return
+     */
+    public Double getRpm() {
+		return this.speed / (2 * this.TIRE_RADIUS * Math.PI ) * 60;
+	}
+    
+    /**
+     * Get the number of steps that have passed in the simulation
+     * @return
+     */
+	public int getSteps() {
+		return steps;
+	}
+	
+}

9d1fd03e0c05b07bf36007d2eadc2186ab278884
thans <tore@d-69-91-153-176.dhcp4.washington.edu>
2013-11-21 13:05:48 -0800

    Added jfreechart functionality for the graph

--- src/com/uwecocar2/vehiclesimulation/DriveCycle.java
+++ src/com/uwecocar2/vehiclesimulation/DriveCycle.java
@@ -3,12 +3,15 @@ package com.uwecocar2.vehiclesimulation;
 import com.uwecocar2.vehiclesimulation.utils.LookUpTable1D;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class DriveCycle implements Driver {
     private LookUpTable1D gasPedalPositions;
     private LookUpTable1D brakePedalPosition;
+    private String filename;
 
     public DriveCycle(File file) throws Exception {
+    	filename = file.getName().split(".txt")[0];
         gasPedalPositions = new LookUpTable1D(file, 0, 1);
         brakePedalPosition = new LookUpTable1D(file, 0, 2);
     }
@@ -26,4 +29,9 @@ public class DriveCycle implements Driver {
         }
         return brakePedalPosition.getValue(time);
     }
+
+	@Override
+	public String getName() {
+		return this.filename;
+	}
 }
--- src/com/uwecocar2/vehiclesimulation/ui/JGraph.java
+++ src/com/uwecocar2/vehiclesimulation/ui/JGraph.java
@@ -3,22 +3,34 @@ package com.uwecocar2.vehiclesimulation.ui;
 import com.uwecocar2.vehiclesimulation.Recorder;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.text.DecimalFormat;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class JGraph extends JPanel {
 
 	// the data to graph
 	private Recorder data;
+	private String yAxis;
+	XYSeriesCollection dataCollection;
 	
 	// the graph padding
 	private static final int OFFSET_X = 75;
 	private static final int OFFSET_Y = 75;
-	private static final int SIZE = 2;
+	private static final int SIZE = 4;
 	private static final int FONT_SIZE = 20;
 	
 	private static final DecimalFormat DF = new DecimalFormat("###.##");
@@ -29,6 +41,47 @@ public class JGraph extends JPanel {
 		super();
 		this.setFont(FONT);
 		METRICS = this.getFontMetrics(FONT);
+		dataCollection = new XYSeriesCollection();
+	}
+	
+	public JPanel getChart() {
+		XYSeries series = new XYSeries("Efficiency");
+		int index = 0;
+		
+		int width = this.getWidth() - OFFSET_X;
+		int height = this.getHeight() - OFFSET_Y;
+		
+		double xMult;
+        double yMult;
+        try {
+            xMult = ((double)width - OFFSET_X)/this.data.size();
+            yMult = ((double)height - OFFSET_Y)/this.data.max();
+        } catch (Exception e) {
+            xMult = 0;
+            yMult = 0;
+        }
+		
+		
+		for(Double value : this.data) {
+			int x = (int)(index*xMult) + OFFSET_X;
+			int y = (int)(value*yMult) - OFFSET_Y;
+			series.add(x, y);
+			index++;
+		}
+		dataCollection.addSeries(series);
+		JFreeChart lineChartObject=ChartFactory.createXYLineChart("Efficiency", "Time", yAxis, dataCollection);
+		//JFreeChart lineChartObject=ChartFactory.createLineChart("title","x","y", data,PlotOrientation.VERTICAL,true,true,false);
+		ChartPanel chPanel = new ChartPanel(lineChartObject); //creating the chart panel, which extends JPanel
+		
+		chPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 10));
+		chPanel.setLayout(new BoxLayout(chPanel, BoxLayout.Y_AXIS));
+		
+		//chPanel.setPreferredSize(new Dimension(400, 400)); //size according to my window
+		chPanel.setMouseWheelEnabled(true);
+		chPanel.setVisible(true);
+		JPanel containingPanel = new JPanel();
+		containingPanel.add(chPanel);
+		return containingPanel;
 	}
 	
 	@Override
@@ -39,6 +92,7 @@ public class JGraph extends JPanel {
 		g.clearRect(0, 0, this.getWidth(), this.getHeight());
 		
 		g.setColor(Color.BLACK);
+		// draw the x and y axis
 		g.drawLine(OFFSET_X, OFFSET_Y, OFFSET_X, height);
 		g.drawLine(OFFSET_X, height, width, height);
 		
@@ -53,12 +107,22 @@ public class JGraph extends JPanel {
         }
 		
 		int index = 0;
+		double prev = 0.0;
 		for(Double value : data) {
 			int x = (int)(index*xMult) + OFFSET_X;
 			int y = (int)(value*yMult) - OFFSET_Y;
+			if (value < prev) {
+				g.setColor(Color.RED);
+			} else {
+				g.setColor(Color.BLACK);
+			}
 			g.fillOval(x - SIZE/2, height - y - OFFSET_Y - SIZE/2, SIZE, SIZE);
 			index++;
-		}		
+			
+			prev = value;
+		}
+		g.setColor(Color.BLACK);
+
 		g.drawString("0", OFFSET_X - FONT_SIZE, height + FONT_SIZE);
 		if (data.size() > 1) {
 			g.drawString(Integer.toString(data.size()),(int)(xMult * (data.size() - 1)) + OFFSET_X, height + FONT_SIZE);
@@ -70,9 +134,12 @@ public class JGraph extends JPanel {
 	 * Set the data to graph
 	 * @param data
 	 */
-	public void setData(Recorder data) {
+	public JPanel setData(Recorder data, String yAxis) {
 		this.data = data;
-		this.repaint();
+		this.yAxis = yAxis;
+		this.dataCollection.removeAllSeries();
+		return this.getChart();
+		//this.repaint();
 	}
 
 }
